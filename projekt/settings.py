381eadc8f2deeed22d300658cd6fd632810da6a0
kamsza <kamsza@onet.pl>
2020-05-25 19:27:39 +0200

    database changed to PostgreSQL

--- projekt/settings.py
+++ projekt/settings.py
@@ -79,8 +79,12 @@ WSGI_APPLICATION = 'projekt.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'ztdns',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': '185.243.53.245',
+        'PORT': '5432'
     }
 }
 
