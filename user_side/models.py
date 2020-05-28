23d8e89c449e7dbc8f515e1bea824fc30f302033
kamsza <kamsza@onet.pl>
2020-05-28 16:58:10 +0200

    database v1 created

--- user_side/models.py
+++ user_side/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Service(models.Model):
+    name = models.CharField(max_length=100)
+    IP = models.GenericIPAddressField()
+
+class DNS(models.Model):
+    label = models.CharField(max_length=100)
+    location = models.CharField(max_length=100)
+    IP = models.GenericIPAddressField()
