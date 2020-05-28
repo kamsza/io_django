54f65c2019133dac1ebc348b5526d61fd7275d21
kamsza <kamsza@onet.pl>
2020-05-28 17:15:13 +0200

    database v2 created

--- user_side/models.py
+++ user_side/models.py
@@ -11,3 +11,13 @@ class DNS(models.Model):
     label = models.CharField(max_length=100)
     location = models.CharField(max_length=100)
     IP = models.GenericIPAddressField()
+    
+class Service_DNS(models.Model):
+    service = models.ForeignKey('Service', on_delete=models.SET_NULL)
+    dns = models.ForeignKey('DNS', on_delete=models.SET_NULL, null=True)
+
+class History(models.Model):
+    service = models.ForeignKey('Service', on_delete=models.CASCADE)
+    dns = models.ForeignKey('DNS', on_delete=models.SET_NULL, null=True)
+    date = models.DateTimeField(default=timezone.now)
+    result = models.CharField(max_length=50)
