f80f1be8e1741d7ba39397872d9f96f81aba4df9
kamsza <kamsza@onet.pl>
2020-05-28 23:17:08 +0200

    give admin full access to database

--- user_side/admin.py
+++ user_side/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+
+admin.site.register(Service)
+admin.site.register(DNS)
+admin.site.register(Location)
+admin.site.register(Service_DNS)
+admin.site.register(History)
+admin.site.register(Subscription)
+admin.site.register(Order)
