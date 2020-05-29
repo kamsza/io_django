07f86f26e851c1a300af76201fe2234e3c52bfb9
Wojciech Kosior <kwojtus@protonmail.com>
2020-05-29 12:07:51 +0200

    also install/uninstall python scripts

--- install.sh
+++ install.sh
@@ -16,5 +16,12 @@ fi
 # they're not to be executed directly by the user
 install -D -m744 vpn_wrapper.sh "$INSTALL_ROOT"/var/lib/0tdns/vpn_wrapper.sh
 install -D -m744 netns-script "$INSTALL_ROOT"/var/lib/0tdns/netns-script
+install -D -m755 perform_queries.py "$INSTALL_ROOT"/var/lib/0tdns/perform_queries.py
+
+# This one would make sense to be executed directly, so it'll go to sbin
+install -D -m744 hourly.py "$INSTALL_ROOT"/usr/sbin/hourly.py
+
+# This is the script, that will get called by cron
+install -D -m744 hourly.sh "$INSTALL_ROOT"/usr/sbin/hourly.sh
 
 # simillar approach will be used to install other files
--- uninstall.sh
+++ uninstall.sh
@@ -9,4 +9,5 @@ userdel 0tdns
 if [ "x$1" = "x--delete-files" ]; then
     rm -r /var/lib/0tdns/
     rm -r /etc/netns/0tdns*
+    rm /usr/sbin/hourly.sh /usr/sbin/hourly.py
 fi
