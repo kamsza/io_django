251181f7e5d995e50736d6a86d29c3917e64af08
Wojciech Kosior <kwojtus@protonmail.com>
2020-05-29 12:52:03 +0200

    leave postgres management to django

--- cleanup_postgres.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-echo "Dropping 'ztdns' database and role"
-cat | sudo -u postgres psql <<EOF
-drop database ztdns;
-drop role ztdns;
-EOF
--- setup_postgres.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-echo "Creating 'ztdns' role"
-sudo -u postgres createuser --no-createdb --no-createrole \
-     --no-superuser --pwprompt ztdns
-
-echo "Creating 'ztdns' database"
-sudo -u postgres createdb -O ztdns ztdns
